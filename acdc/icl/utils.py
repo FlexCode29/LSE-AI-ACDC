9c4bbb0bd4f80252162dc046deb5b8aa3963da6c
Marco Molinari <mmolimari@gmail.com>
2024-02-21 00:22:32 +0000

    ACDC running

--- acdc/icl/utils.py
+++ acdc/icl/utils.py
@@ -5,6 +5,16 @@ from acdc.docstring.utils import AllDataThings
 import torch.nn.functional as F
 from numpy import array
 
+
+def get_gpt2(device="cpu"):
+    tl_model = HookedTransformer.from_pretrained("gpt2")
+    tl_model = tl_model.to(device)
+    tl_model.set_use_attn_result(True)
+    tl_model.set_use_split_qkv_input(True)
+    if "use_hook_mlp_in" in tl_model.cfg.to_dict():
+        tl_model.set_use_hook_mlp_in(True)
+    return tl_model
+
 def generate_data(model, device, x_initial, y_initial, icl_length, n, offset=0):
     prompts = []
     correct_answers = []
@@ -34,14 +44,32 @@ def generate_data(model, device, x_initial, y_initial, icl_length, n, offset=0):
     return prompts, correct_answers_tensor
 
 
-def validation_metric(model, model_output, correct, return_one_element, device):
 
-        model_output = model_output.to(device)
+
+def get_all_icl_things(device='cpu', x=2, y=1, icl_length=12, n=10, return_one_element: bool = False) -> AllDataThings:
+    model = get_gpt2(device)
+
+    # Generate validation data
+    validation_data, validation_correct_answers = generate_data(model, device, x, y, icl_length, n, offset=0)
+
+    # Generate validation patch data with different x and y values (using offset)
+    validation_patch_data, _ = generate_data(model, device, x, y, icl_length, n, offset=n)
+
+    # Generate separate test data with further different x and y values
+    test_data, test_correct_answers = generate_data(model, device, x, y, icl_length, n, offset= n * 2)
+
+    # Generate test patch data with different x and y values (using offset)
+    test_patch_data, _ = generate_data(model, device, x, y, icl_length, n, offset= n * 3)
+
+
+    def validation_metric(output, correct, return_one_element, device):
+
+        output = output.to(device)
     
         # Select the logits for the last token in each sequence
         # model_output shape: [batch_size, seq_length, vocab_size] => [10, 103, 50257]
         # We select [:, -1, :] to get the last token logits for each example in the batch
-        last_token_logits = model_output[:, -1, :]  # Shape: [10, 50257]
+        last_token_logits = output[:, -1, :]  # Shape: [10, 50257]
     
         # Now, find the indices of the 10 highest logits for the last token across the batch
         # We use torch.topk to get the top 10 logits' indices for each example
@@ -54,34 +82,14 @@ def validation_metric(model, model_output, correct, return_one_element, device):
         mse = F.mse_loss(predictions, correct, reduction='mean' if not return_one_element else 'sum')
         return mse
 
-
-
-def get_all_icl_things(device='cpu', x=2, y=1, icl_length=12, n=10, return_one_element: bool = False) -> AllDataThings:
-    model = HookedTransformer.from_pretrained("gpt2").to(device)
-
-    # Generate validation data
-    validation_data, validation_correct_answers = generate_data(model, device, x, y, icl_length, n)
-
-    # Generate validation patch data with different x and y values (using offset)
-    validation_patch_data, _ = generate_data(model, device, x, y, icl_length, n // 2, offset=10)
-
-    # Generate separate test data with further different x and y values
-    test_data, test_correct_answers = generate_data(model, device, x, y, icl_length, n // 2, offset=20)
-
-    # Generate test patch data with different x and y values (using offset)
-    test_patch_data, _ = generate_data(model, device, x, y, icl_length, n // 2, offset=30)
-
-
-    
-
     return AllDataThings(
         tl_model=model,
-        validation_metric=partial(validation_metric, model=model, correct=validation_correct_answers, return_one_element=return_one_element, device=device),
+        validation_metric=partial(validation_metric, correct=validation_correct_answers, return_one_element=return_one_element, device=device),
         validation_data=validation_data,
         validation_labels=validation_correct_answers,
         validation_mask=None,
         validation_patch_data=validation_patch_data,
-        test_metrics=partial(validation_metric, model=model, correct=test_correct_answers, return_one_element=return_one_element, device=device),
+        test_metrics=partial(validation_metric, correct=test_correct_answers, return_one_element=return_one_element, device=device),
         test_data=test_data,
         test_labels=test_correct_answers,
         test_mask=None,
@@ -89,7 +97,8 @@ def get_all_icl_things(device='cpu', x=2, y=1, icl_length=12, n=10, return_one_e
     )
 
 
-# Testing:
+# Testing (pay attention, this is now buggd and would require valid function outside of get things):
+'''
 import unittest
 class TestICLDataAndModel(unittest.TestCase):
     def setUp(self):
@@ -119,3 +128,4 @@ class TestICLDataAndModel(unittest.TestCase):
 # Execute tests when the script is run
 if __name__ == '__main__':
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
+'''
